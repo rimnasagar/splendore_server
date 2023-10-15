699797f825043a7540888bab89b4e299ba980488
AkhilAsok <akhil.asok207@gmail.com>
2023-10-15 11:53:38 +0530

    builk upload fix

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,7 +2,8 @@ const Product = require("../models/Product");
 const { FETCH_SUCCESS, SERVER_ERROR, CREATED_SUCCESS, DELETED_SUCCESS } = require("../messages");
 const { HTTP_STATUS, USER_STATUS } = require("../constants");
 const xlsx = require('xlsx');
-
+const { parse } = require("csv-parse");
+const fs = require("fs");
 
 exports.getAllProducts = async (req, res) => {
     try {
@@ -103,7 +104,7 @@ exports.delete = async (req, res) => {
 
 exports.storeBulkProducts = async (req, res) => {
     try {
-        if (!req.files || !req.files.file) {
+        if (!req.file) {
             return res.status(HTTP_STATUS.BAD_REQUEST).json({
                 status: "error",
                 error: {
@@ -115,23 +116,40 @@ exports.storeBulkProducts = async (req, res) => {
             });
         }
 
-        const file = req.files.file; // Assuming you're using a file upload middleware like `multer`
-
-        const workbook = xlsx.read(file.data, { type: 'buffer' });
-        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-        const products = xlsx.utils.sheet_to_json(worksheet);
-
-        for (const product of products) {
-            // Validate and insert the product data into the database
-            await new Product(product).save();
-        }
-
-        res.status(HTTP_STATUS.CREATED).json({
-            status: "success",
-            error: null,
-            message: "Bulk product upload successful",
-            result: null
-        });
+        const file = req.file;
+        await fs.createReadStream(file.path)
+            .pipe(parse({ delimiter: ",", from_line: 2 }))
+            .on("data", async (row) => {
+                const [ name, original_price, selling_price, discount ] = row;
+                if(name !== "") {
+                    await new Product({
+                        name, 
+                        original_price,
+                        selling_price,
+                        discount
+                    }).save();
+                }
+            })
+            .on("end", async () => {
+                res.status(HTTP_STATUS.CREATED).json({
+                    status: "success",
+                    error: null,
+                    message: "Bulk product upload successful",
+                    result: null
+                });
+            })
+            .on("error", function (error) {
+                console.log(error.message);
+                res.status(HTTP_STATUS.INTERNAL_SERVER).json({
+                    status: "error",
+                    error: {
+                        status: true,
+                        message: SERVER_ERROR,
+                    },
+                    message: SERVER_ERROR,
+                    result: null
+                });
+            });
     } catch (err) {
         console.log(err);
         res.status(HTTP_STATUS.INTERNAL_SERVER).json({
--- routes/bulkadd.js
+++ routes/bulkadd.js
@@ -1,10 +1,12 @@
 const routes = require('express').Router();
+const os = require("os");
 const ProductController = require("../controllers/productController");
-
+const multer  = require('multer');
+const upload = multer({ dest: os.tmpdir() })
 
 routes.get('/', ProductController.getAllProducts);
 routes.get('/:id', ProductController.getProduct);
-routes.post('/', ProductController.storeBulkProducts );
+routes.post('/', upload.single('file'), ProductController.storeBulkProducts );
 routes.delete('/:id', ProductController.delete);
 
 module.exports = routes;
